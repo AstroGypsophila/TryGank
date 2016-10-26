86b13b8c9e12a27eead9b45627bb10987d9d96a8
AstroGypsophila <astro_chenjie@163.com>
2016-10-26 22:46:45 +0800

    using Glide Target listener to download images and send simple data to other Apps

--- TryGankLib/Common/CommonLib/src/main/java/com/gypsophila/commonlib/utils/ImageLoaderUtils.java
+++ TryGankLib/Common/CommonLib/src/main/java/com/gypsophila/commonlib/utils/ImageLoaderUtils.java
@@ -3,22 +3,24 @@ package com.gypsophila.commonlib.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
 import com.gypsophila.commonlib.R;
-import com.orhanobut.logger.Logger;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Description :
@@ -37,54 +39,97 @@ public class ImageLoaderUtils {
         }
     }
 
+    /**
+     * download image
+     *
+     * @param context
+     * @param imageUrl
+     * @param imgName
+     */
     public static void downloadImage(final Context context, final String imageUrl, final String imgName) {
-        new Thread(new Runnable() {
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL) {
             @Override
-            public void run() {
-                Bitmap bitmap = null;
-                try {
-                    if (!TextUtils.isEmpty(imageUrl)) {
-                        bitmap = Glide.with(context)
-                                .load(imageUrl)
-                                .asBitmap()
-                                .into(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-                                .get();
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                if (resource != null) {
+                    File appDir = new File(Environment.getExternalStorageDirectory(),
+                            context.getString(R.string.app_name));
+                    if (!appDir.exists()) {
+                        appDir.mkdir();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (bitmap == null) {
-                        Logger.t("cj_data").w("bitmap == null");
-                    } else {
-                        try {
-                            File appDir = new File(Environment.getExternalStorageDirectory(),
-                                    context.getString(R.string.app_name));
-                            if (!appDir.exists()) {
-                                appDir.mkdir();
-                            }
-                            String fileName = imgName + ".jpg";
-                            File file = new File(appDir, fileName);
-
-                            FileOutputStream fos = new FileOutputStream(file);
-                            assert bitmap != null;
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                            fos.flush();
-                            fos.close();
-                            Logger.t("cj_data").w("file path = " + file.getAbsolutePath());
-                            Uri uri = Uri.fromFile(file);
-                            //通知图库更新
-                            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
-                            context.sendBroadcast(intent);
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    String fileName = imgName + ".jpg";
+                    File file = new File(appDir, fileName);
+                    try {
+                        FileOutputStream fos = new FileOutputStream(file);
+                        resource.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                        fos.flush();
+                        fos.close();
+                        Uri uri = Uri.fromFile(file);
+                        //通知图库更新
+                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+                        context.sendBroadcast(intent);
+                        Toast.makeText(context, (context.getString(R.string.download_picture_success)
+                                + file.getAbsolutePath()), Toast.LENGTH_SHORT).show();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-        }).start();
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                Toast.makeText(context, context.getString(R.string.download_picture_fail),
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        Glide.with(context).load(imageUrl).asBitmap().into(target);
     }
+
+    public interface DownloadImageListener {
+        void loadSuccess(Uri uri);
+
+        void loadFailed(Exception e);
+    }
+    public static void downloadImage(final Context context, final String imageUrl, final String imgName, final DownloadImageListener listener) {
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL) {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                if (resource != null) {
+                    File appDir = new File(Environment.getExternalStorageDirectory(),
+                            context.getString(R.string.app_name));
+                    if (!appDir.exists()) {
+                        appDir.mkdir();
+                    }
+                    String fileName = imgName + ".jpg";
+                    File file = new File(appDir, fileName);
+                    try {
+                        FileOutputStream fos = new FileOutputStream(file);
+                        resource.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                        fos.flush();
+                        fos.close();
+                        Uri uri = Uri.fromFile(file);
+                        listener.loadSuccess(uri);
+                        //通知图库更新
+                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+                        context.sendBroadcast(intent);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                listener.loadFailed(e);
+                Toast.makeText(context, context.getString(R.string.download_picture_fail),
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        Glide.with(context).load(imageUrl).asBitmap().into(target);
+    }
+
+
 }
